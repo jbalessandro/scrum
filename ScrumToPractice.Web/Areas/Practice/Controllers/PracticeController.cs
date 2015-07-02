1bc6f7108902d9bdc2d57225c63462cc7c3aeb1f
jalessandro <jb.alessandro@gmail.com>
2015-07-02 09:10:55 -0300

    Ajax OK, falta apenas ultima questao = true

--- ScrumToPractice.Web/Areas/Practice/Controllers/PracticeController.cs
+++ ScrumToPractice.Web/Areas/Practice/Controllers/PracticeController.cs
@@ -33,8 +33,8 @@ namespace ScrumToPractice.Web.Areas.Practice.Controllers
             {
                 // TODO: voltar aqui
                 // cria um novo simulado
-                //var simulado = cortesia.GetSimulado(cortesia.CriarSimulado());
-                var cortesiaSimulado = cortesia.GetSimulado(6);
+                var simulado = cortesia.GetSimulado(cortesia.CriarSimulado());
+                var cortesiaSimulado = cortesia.GetSimulado(simulado.Cortesia.Id);
                 //questao = cortesia.GetQuestao(cortesiaSimulado.QuestoesSimuladas.ToList().OrderBy(x => x.Id).First().Id);
                 var alternativa = cortesiaSimulado.QuestoesSimuladas.ToList().OrderBy(x => x.Id).First();
                 questao = cortesia.GetQuestao(alternativa.IdCortesia, alternativa.IdQuestao);
@@ -48,14 +48,13 @@ namespace ScrumToPractice.Web.Areas.Practice.Controllers
             return View(questao);
         }
 
-        //public ActionResult Proxima(IEnumerable<int> selecionadas, int idCortesia, int idQuestao)
-        [HttpPost]
-        public ActionResult Next(int idCortesia, int idQuestao, FormCollection collection)
+        //public PartialViewResult Next(int idCortesia, int idQuestao, FormCollection collection)
+        public ActionResult Proxima(IEnumerable<int> selecionadas, int idCortesia, int idQuestao)
         {
             var proximaQuestao = cortesia.GetProximaQuestao(idCortesia, idQuestao);
             // TODO: validar proxima questao....
             // add model error
-            return View("Index", proximaQuestao );
+            return PartialView("_QuestaoCortesia", (ScrumToPractice.Domain.Models.QuestaoCortesia)proximaQuestao);
         }
     }
 }
