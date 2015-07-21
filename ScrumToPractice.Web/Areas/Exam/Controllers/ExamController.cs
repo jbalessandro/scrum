c20c87ca37bb2a2541a08c5815feba56912a71da
jalessandro <jb.alessandro@gmail.com>
2015-07-21 08:03:49 -0300

    Publish

--- ScrumToPractice.Web/Areas/Exam/Controllers/ExamController.cs
+++ ScrumToPractice.Web/Areas/Exam/Controllers/ExamController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using ScrumToPractice.Domain.Abstract;
+﻿using ScrumToPractice.Domain.Abstract;
 using ScrumToPractice.Domain.Models;
 using ScrumToPractice.Domain.Service;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ScrumToPractice.Web.Areas.Exam.Controllers
 {
