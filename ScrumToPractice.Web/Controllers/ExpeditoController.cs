fc8eb802f44729b0f52d4afdd23571871adbeb15
jalessandro <jb.alessandro@gmail.com>
2015-07-29 17:57:15 -0300

    Expedito

---
+++ ScrumToPractice.Web/Controllers/ExpeditoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumToPractice.Web.Controllers
+{
+    public class ExpeditoController : Controller
+    {
+        // GET: Expedito
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
