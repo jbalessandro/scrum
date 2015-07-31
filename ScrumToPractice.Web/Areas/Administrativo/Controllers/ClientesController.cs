7b50a9a30b9a6d055014d30a04f3a48f46f0d4c2
jalessandro <jb.alessandro@gmail.com>
2015-07-31 14:14:24 -0300

    cliente pagamento

---
+++ ScrumToPractice.Web/Areas/Administrativo/Controllers/ClientesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumToPractice.Web.Areas.Administrativo.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Administrativo/Clientes/Pagamentos
+        public ActionResult Pagamentos()
+        {
+            return View();
+        }
+    }
+}
