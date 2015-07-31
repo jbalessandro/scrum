d44cca00050eb8006ff2bafc23931f37af288f7a
jalessandro <jb.alessandro@gmail.com>
2015-07-31 17:21:06 -0300

    CLASSES

---
+++ ScrumToPractice.Domain/Service/ClientePagamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrumToPractice.Domain.Models;
+
+namespace ScrumToPractice.Domain.Service
+{
+    public class ClientePagamento
+    {
+    }
+
+    public class Pagamentos
+    {
+        public Cliente MyProperty { get; set; }
+        public Payment Pagamentos { get; set; }
+    }
+}
